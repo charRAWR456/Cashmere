5f6f69e6c25277f037a278b4f05300d63b16c34c
Guest User <Guest@epicodus-4.local>
2018-04-10 16:26:43 -0700

    add algorithm branch

--- src/app/models/grid.model.ts
+++ src/app/models/grid.model.ts
@@ -11,6 +11,7 @@ export class grid {
     this.setEnemyPosition();
     this.setObstacles();
     this.setGoal();
+    this.enemyRadar();
   }
 
   createBoard() {
@@ -54,7 +55,56 @@ export class grid {
         }
       }
     }
-
+  }
+  findEnemy() {
+    for (let i = 0; i<10; i++) {
+      for (let j = 0; j<10; j++) {
+        if (this.board[i][j].enemy === true) {
+          return this.board[i][j];
+        }
+      }
+    }
+  }
+  findTile(y:number, x:number) {
+  return this.board[x][y];
+  }
+  enemyRadar() {
+    let openList = [];
+    let closedList = [];
+    let currentTile = this.findEnemy();
+    let target = this.findPlayer();
+    closedList.push(currentTile);
+    let addX = currentTile.x+1;
+    let minusX = currentTile.x-1;
+    // let addY = currentTile.y+1;
+    // let minusY = currentTile.y-1;
+    // if (addX !== 10 && addX !== 0 && minusX !== 10 && minusX !== 0 && addY !== 10 && addY !== 0 && minusY !== 10 && minusY !== 0)
+     if(currentTile.x > 9 || currentTile.y > 9 || currentTile.x < 0 || currentTile.y < 0){
+    let upTile = this.findTile(currentTile.x+1, currentTile.y)
+    openList.push(upTile);
+  }
+     if(currentTile.x > 9 || currentTile.y > 9 || currentTile.x < 0 || currentTile.y < 0){
+       let leftTile = this.findTile(currentTile.x, currentTile.y+1)
+    openList.push(leftTile);
   }
 
-}
+     if(currentTile.x > 9 || currentTile.y > 9 || currentTile.x < 0 || currentTile.y < 0){
+       let rightTile = this.findTile(currentTile.x, currentTile.y-1)
+    openList.push(rightTile);
+  }
+     if(currentTile.x > 9 || currentTile.y > 9 || currentTile.x < 0 || currentTile.y < 0){
+       let bottomTile = this.findTile(currentTile.x-1, currentTile.y)
+    openList.push(bottomTile);
+  }
+    console.log(openList);
+    return openList;
+    }
+
+    tileCount(height: number, width: number){
+    let gridSize: number = width * height;
+
+    return gridSize;
+    }
+
+
+  }
