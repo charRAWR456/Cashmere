1ed2d14d7c63ceb9ea26d8f2e9aae34c7ec0724b
Panatda Inthala <panatdainthala@gmail.com>
2018-04-10 21:09:19 -0700

    finds surrounding tiles

--- src/app/models/grid.model.ts
+++ src/app/models/grid.model.ts
@@ -1,11 +1,11 @@
-import { Tile } from './tile.model';
+import { Tile } from "./tile.model";
 
 export class grid {
   board = [];
   height: number = 10;
   width: number = 10;
 
-  constructor(){
+  constructor() {
     this.createBoard();
     this.setPlayerPosition();
     this.setEnemyPosition();
@@ -18,18 +18,18 @@ export class grid {
     for (var i = 0; i < this.width; i++) {
       this.board[i] = [];
       for (var j = 0; j < this.height; j++) {
-        this.board[i][j] = new Tile(i,j);
+        this.board[i][j] = new Tile(i, j);
       }
     }
   }
 
-  setPlayerPosition(){
+  setPlayerPosition() {
     this.board[9][1].player = true;
   }
-  setEnemyPosition(){
+  setEnemyPosition() {
     this.board[9][9].enemy = true;
   }
-  setObstacles(){
+  setObstacles() {
     this.board[5][2].walkable = false;
     this.board[3][5].walkable = false;
     this.board[9][0].walkable = false;
@@ -43,13 +43,13 @@ export class grid {
     this.board[7][1].walkable = false;
     this.board[7][8].walkable = false;
   }
-  setGoal(){
-    this.board[0][9].goal=true;
+  setGoal() {
+    this.board[0][9].goal = true;
   }
 
   findPlayer() {
-    for (let i = 0; i<10; i++) {
-      for (let j = 0; j<10; j++) {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
         if (this.board[i][j].player === true) {
           return this.board[i][j];
         }
@@ -57,54 +57,54 @@ export class grid {
     }
   }
   findEnemy() {
-    for (let i = 0; i<10; i++) {
-      for (let j = 0; j<10; j++) {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
         if (this.board[i][j].enemy === true) {
           return this.board[i][j];
         }
       }
     }
   }
-  findTile(y:number, x:number) {
-  return this.board[x][y];
+  findTile(x: number, y: number) {
+    if (this.board.length > x && this.board[x].length > y) {
+      return this.board[x][y];
+    }
   }
   enemyRadar() {
     let openList = [];
     let closedList = [];
     let currentTile = this.findEnemy();
     let target = this.findPlayer();
+    console.log("currentTile", currentTile);
+    console.log("target", target);
+
     closedList.push(currentTile);
-    let addX = currentTile.x+1;
-    let minusX = currentTile.x-1;
+    let addX = currentTile.x + 1;
+    let minusX = currentTile.x - 1;
     // let addY = currentTile.y+1;
     // let minusY = currentTile.y-1;
     // if (addX !== 10 && addX !== 0 && minusX !== 10 && minusX !== 0 && addY !== 10 && addY !== 0 && minusY !== 10 && minusY !== 0)
-     if(currentTile.x > 9 || currentTile.y > 9 || currentTile.x < 0 || currentTile.y < 0){
-    let upTile = this.findTile(currentTile.x+1, currentTile.y)
-    openList.push(upTile);
-  }
-     if(currentTile.x > 9 || currentTile.y > 9 || currentTile.x < 0 || currentTile.y < 0){
-       let leftTile = this.findTile(currentTile.x, currentTile.y+1)
-    openList.push(leftTile);
-  }
+    const down = this.findTile(currentTile.x, currentTile.y + 1);
+    const up = this.findTile(currentTile.x, currentTile.y - 1);
+    const left = this.findTile(currentTile.x - 1, currentTile.y);
+    const right = this.findTile(currentTile.x + 1, currentTile.y);
 
-     if(currentTile.x > 9 || currentTile.y > 9 || currentTile.x < 0 || currentTile.y < 0){
-       let rightTile = this.findTile(currentTile.x, currentTile.y-1)
-    openList.push(rightTile);
-  }
-     if(currentTile.x > 9 || currentTile.y > 9 || currentTile.x < 0 || currentTile.y < 0){
-       let bottomTile = this.findTile(currentTile.x-1, currentTile.y)
-    openList.push(bottomTile);
-  }
-    console.log(openList);
+    console.log("down", down);
+    console.log("up", up);
+    console.log("left", left);
+    console.log("right", right);
+    if (up && up.walkable) openList.push(up);
+    if (down && down.walkable) openList.push(down);
+    if (left && left.walkable) openList.push(left);
+    if (right && right.walkable) openList.push(right);
+
+    console.log("move", openList);
     return openList;
-    }
+  }
 
-    tileCount(height: number, width: number){
+  tileCount(height: number, width: number) {
     let gridSize: number = width * height;
 
     return gridSize;
-    }
-
-
   }
+}
